cb0f5ce61ec7a7d93945117402deb75374e8a8d2
Sneha Tiwari <snehat1806@gmail.com>
2026-05-06 22:59:55 +0530

    updated version

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import re
 from datetime import datetime
 
 FILE_NAME = "students.json"
@@ -22,8 +23,8 @@ def save_data(data):
 # ----------------- Validation -----------------
 
 def validate_email(email):
-    return "@" in email and "." in email
-
+    pattern=r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(pattern, email) is not None
 
 def validate_phone(phone):
     return phone.isdigit() and len(phone) == 10
