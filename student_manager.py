4eaca5c55bbbb394ee0d03e2914d035392b3e2b0
Sneha Tiwari <snehat1806@gmail.com>
2026-05-08 05:46:09 +0530

    updated from json to oops concep's

---
+++ student_manager.py
@@ -0,0 +1,112 @@
+import json
+import re
+from student import Student
+
+FILE_NAME = "students.json"
+
+
+class StudentManager:
+
+    def load_data(self):
+        try:
+            with open(FILE_NAME, "r") as file:
+                return json.load(file)
+        except:
+            return []
+
+    def save_data(self, data):
+        with open(FILE_NAME, "w") as file:
+            json.dump(data, file, indent=4)
+
+    def get_next_id(self, students):
+        if not students:
+            return 101
+        return max(student["id"] for student in students) + 1
+    
+    def validate_email(self, email):
+      pattern = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
+      return re.fullmatch(pattern, email) is not None
+    
+    def list_students(self):
+       students = self.load_data()
+
+       if not students:
+          print("No students found.")
+          return
+
+       print("\n===== STUDENT LIST =====")
+
+       for student in students:
+         print(
+            f'ID: {student["id"]} | '
+            f'Name: {student["name"]} | '
+            f'Department: {student["dept"]}'
+            print(f'Year: {student["year"]}')
+            print(f'Email: {student["email"]}')
+            print(f'Phone: {student["phone"]}')
+  )
+    def search_student(self):
+      students = self.load_data()
+
+      keyword = input("Enter student name to search: ").lower()
+
+      found = False
+
+      for student in students:
+           if keyword in student["name"].lower():
+            print(
+                f'ID: {student["id"]} | '
+                f'Name: {student["name"]} | '
+                f'Department: {student["dept"]}'
+            )
+            found = True
+
+      if not found:
+        print("No matching student found.")
+
+    def add_student(self):
+        students = self.load_data()
+
+        sid = self.get_next_id(students)
+
+        print(f"Auto Generated Student ID: {sid}")
+
+        name = input("Name: ")
+        dept = input("Department: ")
+        year = int(input("Year: "))
+        email = input("Email: ")
+        phone = input("Phone: ")
+
+        if not self.validate_email(email):
+         print("Invalid email format!")
+         return
+        
+        if self.is_duplicate(students, email, phone):
+           return False
+        
+        student = Student(
+            sid,
+            name,
+            dept,
+            year,
+            email,
+            phone
+        )
+
+        students.append(student.to_dict())
+
+        self.save_data(students)
+
+        print("Student added successfully!")
+
+    def is_duplicate(self, students, email, phone):
+      for student in students:
+        if student["email"] == email:
+            print("Email already exists!")
+            return True
+
+        if student["phone"] == phone:
+            print("Phone number already exists!")
+            return True
+
+      return False
